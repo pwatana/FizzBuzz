c57a8461e3356836063b51722a556cf6d6216978
pwatana <patricia.watanabe@mail.utoronto.ca>
2024-09-10 10:38:01 -0400

    new file with the correct reduce code

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
-
 public class Reduce {
+    public static void main(String[] args) {
+        int n = 1000;  // Change this to any number
+
+        // Call the method to process the number
+        processNumber(n);
+    }
+
+    public static void processNumber(int n) {
+        int steps = 0; // To count the number of steps
+
+        // Repeat until n becomes 0
+        while (n > 0) {
+            if (n % 2 == 0) {
+                n = n / 2;
+            } else {
+                n = n - 1;
+            }
+            steps++;
+        }
+
+        // Print the result
+        System.out.println("The number of steps: " + steps);
+    }
 }
